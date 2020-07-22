ca4a78a24bf3e4d9bf4a8e4d83be40351c29daf9
danish45007 <myemail>
2020-07-22 19:26:00 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+
+
+// allow cross-origin-request
+app.use(cors());
 
 // mongoDb connection
 // DB Connection
