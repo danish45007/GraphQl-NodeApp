f8762414644d1e94ee5cd5370cf34efbac1d23d3
danish45007 <myemail>
2020-07-22 19:24:16 +0530

    Book list component added

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react';
 import BookList from './components/BookList';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from 'react-apollo';
+
 // apollo client 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://localhost:4000/graphql'
 });
-
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <div id="main">
-        <BookList />
+        <BookList  />
       </div>
     </ApolloProvider>
   );
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { gql } from 'apollo-boost';
+import { graphql } from 'react-apollo';
 
+// template string
+const getBooksQuery = gql`{
+    books{
+      name
+      genre
+      author {
+        name
+        age
+      }
+    }
+  }
+`
 
-function BookList() {
-  return (
-    <div>
-        <ul id="books-list">
-            <li>Book Name</li>
-        </ul>
-    </div>
-  );
+// binding query to component
+class BookList extends Component {
+    displayBooks(){
+        const data = this.props.data
+        if(data.loading){
+            return(
+                <div>
+                    Loading Books.... 
+                </div>
+            )
+        } else{
+            return data.books.map((book,index) => {
+                return (
+                    <li key={index}>
+                        {book.name}
+                    </li>
+                );
+            })
+        }
+    }
+    render() {
+        // console.log(this.props)
+        return (
+            <div>
+                <ul id="book-list">
+                    {this.displayBooks()}
+                </ul>
+            </div>
+
+        )
+    }
 }
 
-export default BookList;
+export default graphql(getBooksQuery)(BookList);
